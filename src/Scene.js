afd4ee50a6e907cf7ad03ed4c898ee24dac3104d
Jose Hurtarte <JJHH121106@gmail.com>
2021-04-12 13:50:29 -0600

    navBar half complete

---
+++ src/Scene.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.scss';
+import NavBar from './components/NavBar'
+
+
+function Scene(){
+
+    return (
+        <div>
+            <NavBar/>
+        </div>
+
+        
+    );
+}
+
+export default Scene;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import "./style.scss"
 import "bootstrap/dist/js/bootstrap"
 import React from 'react';
 import ReactDom from 'react-dom';
+import Scene from './Scene.js';
 
+ReactDom.render(
+    <React.StrictMode>
+        <Scene/>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
