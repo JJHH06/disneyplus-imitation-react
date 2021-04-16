e6440d6bcd7ab36c696766d4f73a5f1f49be3b61
Jose Hurtarte <JJHH121106@gmail.com>
2021-04-16 13:17:08 -0600

    styles bug fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,15 @@ const Footer = () => {
   return (
     <div className="page-footer">
         <img className='imagen-logo-footer footer-item' src= {logo}/>
-        <p className='footer-item'>Politica de privacidad&nbsp;&nbsp;&nbsp;&nbsp;Acuerdo de suscripcion ayuda&nbsp;&nbsp;&nbsp;&nbsp;Dispositivos compatibles</p>
+        <p className='footer-item'>
+           
+            Politica de privacidad
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            Acuerdo de suscripcion
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            Ayuda
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            Dispositivos compatibles</p>
         <p className='footer-item'>Acerca de Disney+&nbsp;&nbsp;&nbsp;&nbsp;Publicidad personalizada</p>
         <p  className='footer-item'>El servicio Disney+ es comercializado por Disney DTC LATAM, Inc., 2400 W Alameda Ave., Burbank CA 91521 y Tax ID 75-3016153.</p>
         <p   className='footer-item'>© Disney. Todos los derechos reservados.</p>
