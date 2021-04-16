57ede012fbd97f0e5302af59d8a8329bfb292bd4
Jose Hurtarte <JJHH121106@gmail.com>
2021-04-16 05:28:19 -0600

    mapping from single slider working

--- src/components/ContentChooser/SimpleSlider.js
+++ src/components/ContentChooser/SimpleSlider.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import VideosContainer from './VideosContainer';
+import novedades1 from '../../assets/Videos/Novedades/novedades1.jfif';
+import novedades2 from '../../assets/Videos/Novedades/novedades2.jfif';
+import novedades3 from '../../assets/Videos/Novedades/novedades3.jfif';
+import novedades4 from '../../assets/Videos/Novedades/novedades4.jfif';
+import novedades5 from '../../assets/Videos/Novedades/novedades5.jfif';
 
+const videosxd = [novedades1,novedades2,novedades3,novedades4,novedades5];
 
 export default class SimpleSlider extends Component {
   render() {
@@ -16,22 +22,10 @@ export default class SimpleSlider extends Component {
       <div className = "">
         <Slider {...settings}>
           <div>
-            <VideosContainer/>
+            <VideosContainer videos = {videosxd}/>
           </div>
           <div>
-          <VideosContainer/>
-          </div>
-          <div>
-          <VideosContainer/>
-          </div>
-          <div>
-          <VideosContainer/>
-          </div>
-          <div>
-          <VideosContainer/>
-          </div>
-          <div>
-          <VideosContainer/>
+          <VideosContainer videos = {videosxd}/>
           </div>
         </Slider>
       </div>
